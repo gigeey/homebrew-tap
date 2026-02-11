edeba07dd11455f9c0aced2b724395374809437d
eandrew45 <ebongandrew.ea@gmail.com>
2026-02-11 09:04:21 -0800

    update version to 0.2.9

--- Formula/studio-cli.rb
+++ Formula/studio-cli.rb
@@ -1,16 +1,16 @@
 class StudioCli < Formula
   desc "Interactive terminal-based project management system with integrated AI agent capabilities"
   homepage "https://github.com/gigeey/studio-cli-releases"
-  version "0.2.8"
+  version "0.2.9"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/gigeey/studio-cli-releases/releases/download/v#{version}/studio-cli-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "72987314c75cecc7f0ed927df0bba4d778bd9c2bb1b559d5db10c0e9d1faabbc"
+      sha256 "9b97f005cbf6e479c280bf6226566d242da6be645e976bc9263edb5ba99190d2"
     elsif Hardware::CPU.arm?
       url "https://github.com/gigeey/studio-cli-releases/releases/download/v#{version}/studio-cli-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "367082139e595e98c192ffacaaf1c307e096861d9fdf99303f91a73c41fd0e12"
+      sha256 "8df0f80ba8a5518e373649745e7969dbb0bece9399414a3f3347b8fe998c2ac9"
     end
   end
 
